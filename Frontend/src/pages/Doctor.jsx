973f6b648d393317f1f6054d1296378ae04b9fc5
Ketan9548 <kc918062@gmail.com>
2025-03-29 12:48:27 +0530

    Done changes

--- Frontend/src/pages/Doctor.jsx
+++ Frontend/src/pages/Doctor.jsx
@@ -8,12 +8,13 @@ const Doctor = () => {
   const [filter, setfilter] = useState([]);
 
   const { doctors } = useContext(AppContextProvider);
+  console.log(speciality)
 
   const filterapply = () => {
     if (speciality) {
       setfilter(doctors.filter((doctor) => doctor.speciality === speciality));
     }
-    else if (!speciality) {
+    else {
       setfilter(doctors);
     }
   }
@@ -31,8 +32,8 @@ const Doctor = () => {
           <div className="md:col-span-1 bg-white p-6 rounded-xl shadow-md">
             <h2 className="text-xl font-semibold text-gray-700 mb-4">Specialties</h2>
             <div className="space-y-3">
-              {["General Physician", "Gynecologist", "Dermatologist", "Pediatricians", "Neurologist", "Gastroenterologist"].map((spec, index) => (
-                <p key={index} className="bg-blue-100 px-4 py-2 rounded-md text-center text-gray-600 hover:bg-blue-200 cursor-pointer transition">
+              {["General physician", "Gynecologist", "Dermatologist", "Pediatricians", "Neurologist", "Gastroenterologist"].map((spec, index) => (
+                <p onClick={() => speciality === spec ? navigate('/alldoctor') : navigate(`/alldoctor/${spec}`)} key={index} className="bg-blue-100 px-4 py-2 rounded-md text-center text-gray-600 hover:bg-blue-200 cursor-pointer transition">
                   {spec}
                 </p>
               ))}
